ee573b731e70721d2f9415973fac7a4e92c373c8
Noani Barbieri <noanizingerbarbieri@hotmail.com>
2021-04-30 12:24:31 -0300

    Update ProgramaFinal.c

--- ProgramaFinal.c
+++ ProgramaFinal.c
@@ -20,8 +20,8 @@ typedef struct{
     int qtd_paginas;
 }Livros;
 
-int validaISBN(Livros liv[], int *contadorLivros){ //passando por par�metro a struct e um ponteiro que vai apontar para o endere�o da vari�vel do argumento
-    int teste=0, i; //este teste servir� para analisar quantas vezes se repetiu o mesmo ISBN no la�o abaixo
+int validaISBN(Livros liv[], int *contadorLivros){ //passando por parametro a struct e um ponteiro que vai apontar para o endereço da variavel do argumento
+    int teste=0, i; //este teste servira para analisar quantas vezes se repetiu o mesmo ISBN no la�o abaixo
     char ISBN[20];
     int aux = *contadorLivros; //determinando um auxiliar para evitar problemas caso o contador seja alterado
 
@@ -29,17 +29,17 @@ int validaISBN(Livros liv[], int *contadorLivros){ //passando por par�metro a 
     fflush(stdin);
     gets(ISBN);
 
-    for(i=0; i<(*contadorLivros); i++){ //o la�o ir� analisar se o mesmo ISBN inserido j� foi inserido
+    for(i=0; i<(*contadorLivros); i++){ //o laço ira analisar se o mesmo ISBN inserido ja foi inserido
         if(strcmp(ISBN, liv[i].ISBN)==0){
-            teste++; //caso encontrar repeti��o essa vari�vel ir� ser alterada
+            teste++; //caso encontrar repetição essa variável irá ser alterada
         }
     }
-    if(teste==0){ //Portanto, se ela for igual a zero significa que n�o houve repeti��o
-        strcpy(liv[aux].ISBN, ISBN); //fun��o para copiar a string inserida pela usu�rio e colar na struct
+    if(teste==0){ //Portanto, se ela for igual a zero significa que não houve repetição
+        strcpy(liv[aux].ISBN, ISBN); //função para copiar a string inserida pela usuário e colar na struct
         return 0;
     }
     else{
-        return 1; //caso a vari�vel for diferente de zero o ISBN j� existe, portanto o programa retorna 1
+        return 1; //caso a variável for diferente de zero o ISBN já existe, portanto o programa retorna 1
     }
 }
 
@@ -47,14 +47,14 @@ void main(){
     setlocale(LC_ALL, "Portuguese");
 
     Livros livros[100];
-    int op=0; //come�ando em zero para sempre entrar no while de in�cio e evitar problemas de valores aleat�rios na vari�vel
+    int op=0; //começando em zero para sempre entrar no while de inicio e evitar problemas de valores aleatorios na variavel
     int contadorLivros=-1; //conta a quantidade de livros
 
-    int teste=0; //vari�vel para testar se existe algum livro com a ISBN inserida
-    int i; //contador para os la�os
-    int validar; //vari�vel para receber o valor de retorno da fun��o que valida o ISBN
+    int teste=0; //variavel para testar se existe algum livro com a ISBN inserida
+    int i; //contador para os laços
+    int validar; //variavel para receber o valor de retorno da função que valida o ISBN
 
-    //VARI�VEIS PARA SERVIREM COMO BUSCA PELO USU�RIO:
+    //VARIAVEIS PARA SERVIREM COMO BUSCA PELO USUARIO:
     char ISBN[20]; //ISBN para ser comparado com todos os livros presentes na biblioteca
     char categoria[51]; //categoria para ser comparado com todos os livros presentes na biblioteca
     char autor[51]; //Autor para ser comparado com todos os livros presentes na biblioteca
@@ -72,9 +72,9 @@ void main(){
         switch(op){
         case 1:
             contadorLivros++;
-            validar = validaISBN(livros, &contadorLivros); //esta vari�vel ir� receber o retorno da fun��o
-                if(validar==0){ //caso o retorno for zero, os dados poder�o ser preenchidos
-                    printf("Entre com o t�tulo do livro: ");
+            validar = validaISBN(livros, &contadorLivros); //esta variavel ira receber o retorno da função
+                if(validar==0){ //caso o retorno for zero, os dados poderão ser preenchidos
+                    printf("Entre com o titulo do livro: ");
                     fflush(stdin);
                     gets(livros[contadorLivros].titulo);
                     printf("Entre com o autor: ");
@@ -90,9 +90,9 @@ void main(){
                     printf("\nLivro Cadastrado com sucesso!!\n");
 
                 }
-            else{ //caso contr�rio o usu�rio n�o poder� preencher os dados, pois esse ISBN j� existe
+            else{ //caso contrario o usuario nao podera preencher os dados, pois esse ISBN ja existe
                 system("cls");
-                printf("\nImposs�vel cadastrar esse livro, j� existe um livro com esse ISBN no acervo\n");
+                printf("\nImpossivel cadastrar esse livro, ja existe um livro com esse ISBN no acervo\n");
                 contadorLivros--;
             }
         break;
@@ -103,19 +103,19 @@ void main(){
             gets(ISBN);
 
             for(i=0; i<contadorLivros+1; i++){
-                if(strcmp(ISBN, livros[i].ISBN)==0){ //fun��o para comparar strings e caso sejam iguais entra na condi��o
+                if(strcmp(ISBN, livros[i].ISBN)==0){ //função para comparar strings e caso sejam iguais entra na condição
                     system("cls");
                     printf("\nTitulo: %s", livros[i].titulo);
                     printf("\nAutor: %s", livros[i].autor);
                     printf("\nCategoria: %s", livros[i].categoria);
                     printf("\nISBN: %s", livros[i].ISBN);
-                    printf("\nQuantidade de p�ginas: %d\n\n", livros[i].qtd_paginas);
-                    teste++; //acrescentando 1 � vari�vel para verificar se existe o ISBN  na lista
+                    printf("\nQuantidade de paginas: %d\n\n", livros[i].qtd_paginas);
+                    teste++; //acrescentando 1 a variavel para verificar se existe o ISBN  na lista
                 }
             }
-            if(teste==0){ //caso n�o houver o usu�rio ser� informado, pois o teste termina nulo
+            if(teste==0){ //caso nao houver o usuario sera informado, pois o teste termina nulo
                 system("cls");
-                printf("\nISBN n�o encontrada no acervo\n");
+                printf("\nISBN nao encontrada no acervo\n");
             }
             teste=0; //voltando para o valor original para novas pesquisas
         break;
@@ -126,17 +126,17 @@ void main(){
             gets(autor);
 
             system("cls");
-            printf("\nEstes s�o os livros escritos por %s:\n ", autor);
+            printf("\nEstes sao os livros escritos por %s:\n ", autor);
             for(i=0; i<contadorLivros+1; i++){
-                if(strcmp(autor, livros[i].autor)==0){ //fun��o para comparar strings e caso sejam iguais entra na condi��o
-                    printf("\nT�tulo: %s", livros[i].titulo);
+                if(strcmp(autor, livros[i].autor)==0){ //funçao para comparar strings e caso sejam iguais entra na condiçao
+                    printf("\nTitulo: %s", livros[i].titulo);
                     printf("\nCategoria: %s", livros[i].categoria);
                     printf("\nISBN: %s", livros[i].ISBN);
-                    printf("\nQuantidade de p�ginas: %d\n\n", livros[i].qtd_paginas);
-                    teste++; //acrescentando 1 � vari�vel para verificar se existe livros do autor  na lista
+                    printf("\nQuantidade de paginas: %d\n\n", livros[i].qtd_paginas);
+                    teste++; //acrescentando 1 a variavel para verificar se existe livros do autor  na lista
                 }
             }
-            if(teste==0){ //caso n�o houver o usu�rio ser� informado, pois o teste termina nulo
+            if(teste==0){ //caso nao houver o usuario sera informado, pois o teste termina nulo
                 printf("\nNenhum livro escrito por esse autor foi encontrado no acervo\n");
             }
             teste=0; //voltando para o valor original para novas pesquisas
@@ -148,42 +148,42 @@ void main(){
             gets(categoria);
 
             system("cls");
-            printf("\nEstes s�o os livros da categoria %s:\n ", categoria);
+            printf("\nEstes sao os livros da categoria %s:\n ", categoria);
             for(i=0; i<contadorLivros+1; i++){
-                if(strcmp(categoria, livros[i].categoria)==0){ //fun��o para comparar strings e caso sejam iguais entra na condi��o
-                    printf("\nT�tulo: %s", livros[i].titulo);
+                if(strcmp(categoria, livros[i].categoria)==0){ //funcao para comparar strings e caso sejam iguais entra na condicao
+                    printf("\nTitulo: %s", livros[i].titulo);
                     printf("\nAutor: %s", livros[i].autor);
                     printf("\nCategoria: %s", livros[i].categoria);
                     printf("\nISBN: %s", livros[i].ISBN);
-                    printf("\nQuantidade de p�ginas: %d\n\n", livros[i].qtd_paginas);
-                    teste++; //acrescentando 1 � vari�vel para verificar se existe livros da categoria  na lista
+                    printf("\nQuantidade de paginas: %d\n\n", livros[i].qtd_paginas);
+                    teste++; //acrescentando 1 a variavel para verificar se existe livros da categoria  na lista
                 }
             }
-            if(teste==0){ //caso n�o houver o usu�rio ser� informado, pois o teste termina nulo
+            if(teste==0){ //caso nao houver o usuario sera informado, pois o teste termina nulo
                 system("cls");
-                printf("\nN�o existe nenhum livro dessa categoria no acervo\n");
+                printf("\nNao existe nenhum livro dessa categoria no acervo\n");
             }
             teste=0; //voltando para o valor original para novas pesquisas
         break;
 
         case 5:
             system("cls");
-            printf("Estes s�o todos os livros presentes no acervo: \n");
+            printf("Estes sao todos os livros presentes no acervo: \n");
             for(i=0; i<contadorLivros+1; i++){
-                printf("\nT�tulo: %s", livros[i].titulo);
+                printf("\nTitulo: %s", livros[i].titulo);
                 printf("\nAutor: %s", livros[i].autor);
                 printf("\nCategoria: %s", livros[i].categoria);
                 printf("\nISBN: %s", livros[i].ISBN);
-                printf("\nQuantidade de p�ginas: %d\n\n", livros[i].qtd_paginas);
+                printf("\nQuantidade de paginas: %d\n\n", livros[i].qtd_paginas);
             }
         break;
 
         case 6:
             system("cls");
-            printf("\nVoc� optou por encerrar o programa\n");
+            printf("\nVoce optou por encerrar o programa\n");
             exit(0);
         break;
-						        default:
+        default:
             system("cls");
             printf("Error\n");
             printf("Tente novamente.\n");
